064319b205cd55d14efa99c197c219515852d76f
Tuncay Kaplan <trkaplan@gmail.com>
2022-05-15 22:14:31 +0300

    update aggreement ipfs hash

--- constants/other.ts
+++ constants/other.ts
@@ -3,7 +3,7 @@ import { addresses } from '../data/whiteListedAddresses';
 
 export const MAX_NFT_TO_MINT = Object.values(addresses).reduce((prev, curr) => prev + curr, 0);
 
-const AGREEMENT_IPFS_HASH = 'QmUbFb12ZEAyoqGUEsnS8fxh78nowEqNwvn7BbAfryRRay';
+const AGREEMENT_IPFS_HASH = 'QmeHXkNPh3Sr9KwZkL1q8HSMvA3s3LSPqWodSCnZnvqST2';
 export const AGREEMENT_IPFS_URL = `https://ipfs.io/ipfs/${AGREEMENT_IPFS_HASH}`;
 
 export const PARCEL0_NFT_CONTRACT_ADDRESSES: { [key: number]: string } = {
