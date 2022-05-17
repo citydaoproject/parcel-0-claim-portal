143a67a221440da14302d6580a2b31db0d59b438
Tuncay Kaplan <trkaplan@gmail.com>
2022-05-18 00:27:30 +0300

    update description + add ethscan link to success popup

--- components/ClaimSuccessModal/ClaimSuccessModal.tsx
+++ components/ClaimSuccessModal/ClaimSuccessModal.tsx
@@ -5,24 +5,34 @@ import { useModal } from '../../hooks/useModal';
 
 type ClaimSuccessModalProps = {
   eligibleNftsCount: number;
+  txId: string;
 };
-export const ClaimSuccessModal: FC<ClaimSuccessModalProps> = ({ eligibleNftsCount }) => {
+export const ClaimSuccessModal: FC<ClaimSuccessModalProps> = ({ eligibleNftsCount, txId }) => {
   const { isClaimSuccessModalOpen, handleCloseClaimSuccessModal } = useModal();
+  const etherscanUrl = `https://etherscan.io/tx/${txId}`;
   return (
     <Modal
       isOpen={isClaimSuccessModalOpen}
       onRequestClose={handleCloseClaimSuccessModal}
       overlayClassName="react-modal-overlay"
-      className="react-modal-content"
+      className="react-modal-content success-popup"
     >
       <div className="claim-popup">
         <img className="parcel-0-logo-md" src="/citydao-parcel-0-logo-md.png" alt="Parcel Zero NFT" />
         <div className="popup-title">Parcel 0 Claim</div>
-        <div className="popup-content">
-          Success! You just claimed
-          <span className="text-primary">
-            {` ${eligibleNftsCount}`} Parcel 0 NFT{eligibleNftsCount > 1 ? 's' : ''}.
-          </span>
+        <div className="popup-content text-small">
+          <div className="success-message">
+            Congratulations! Your transaction for minting{' '}
+            <span className="text-primary">
+              {` ${eligibleNftsCount}`} Parcel 0 NFT{eligibleNftsCount > 1 ? 's' : ''}
+            </span>{' '}
+            has been submitted!
+            <br /> You may leave this page. {`Your ${eligibleNftsCount > 1 ? 'NFTs' : 'NFT'}`} will be in your wallet as
+            soon as the transaction is processed. In the meantime, you can{' '}
+            <a target="_blank" href={etherscanUrl} rel="noreferrer">
+              check the status here
+            </a>
+          </div>
           <img
             className="mintedNftImage in-success-popup"
             alt="Parcel 0 NFT Art"
