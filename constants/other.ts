3d7d081cbd03b2610fde4225b3d306a5150906ca
Tuncay Kaplan <trkaplan@gmail.com>
2022-05-20 22:44:22 +0300

    update aggreement document URL (typo fix)

--- constants/other.ts
+++ constants/other.ts
@@ -2,7 +2,7 @@ import { addresses } from '../data/whiteListedAddresses';
 
 export const MAX_NFT_TO_MINT = Object.values(addresses).reduce((prev, curr) => prev + curr, 0);
 
-const AGREEMENT_IPFS_HASH = 'QmeHXkNPh3Sr9KwZkL1q8HSMvA3s3LSPqWodSCnZnvqST2';
+const AGREEMENT_IPFS_HASH = 'QmVorF3YxN6KT4RSqBUHDNM3ti1bSPrdZdQHLaEvQfmNNs';
 export const AGREEMENT_IPFS_URL = `https://ipfs.io/ipfs/${AGREEMENT_IPFS_HASH}`;
 
 export const PARCEL0_NFT_CONTRACT_ADDRESSES: { [key: number]: string } = {
